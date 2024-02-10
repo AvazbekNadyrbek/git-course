0120cd9f490b58956fa08e0829b82ce0f8edf153
Avazbek <spatium.avazbek@gmail.com>
2024-02-10 21:16:29 +0100

    Create welcome git commit

---
+++ Artifact.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.List;
+public class Artifact {
+    public static void main(String[] args) {
+        List<String> arrayAdd = new ArrayList<String>();
+        arrayAdd.add("Avazbek");
+        arrayAdd.add("Avazbek2");
+        arrayAdd.add("Avazbek3");
+
+        String arrayGet = arrayAdd.get(1);
+        System.out.println(arrayGet);
+
+        Solution test = new Solution();
+        System.out.println(test.x);
+
+        Solution.InnerClass test2 = new Solution().new InnerClass();
+        System.out.println(test2.x);
+    }
+}
