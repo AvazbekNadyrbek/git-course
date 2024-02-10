0365c2165ddb3e737b33c6835844626d47217f8d
Avazbek <spatium.avazbek@gmail.com>
2024-02-10 22:27:55 +0100

    Add some Java code

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.readme();
+    }
+    public static void readme() {
+        System.out.println("hello");
+    }
+}
